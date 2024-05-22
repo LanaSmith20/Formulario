957d820d51909f3c1a15d67f2e41e4399566f6d2
LanaSmith20 <168363622+LanaSmith20@users.noreply.github.com>
2024-05-22 12:48:48 +0100

    Delete Formulario directory

--- Formulario/app.js
+++
@@ -1,6 +0,0 @@
-function enviar(){
-    let nome = document.querySelector('#nome').value;
-
-    document.querySelector('#resultado').innerHTML = 'O nome retirado da caixa de texto foi:' + nome;
-    
-}
